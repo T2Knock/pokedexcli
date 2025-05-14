ff930899fb1e4631922b4d493562bc4d57cf1340
knock-huynh <huynhtransongngoc@gmail.com>
2025-05-14 22:48:34 +0700

    feat(main): add repl logic

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	lower := strings.ToLower(text)
+	words := strings.Fields(lower)
+
+	return words
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			continue
+		}
+
+		input := scanner.Text()
+		parseInput := cleanInput(input)
+
+		fmt.Printf("Your command was: %v \n", parseInput[0])
+	}
 }
